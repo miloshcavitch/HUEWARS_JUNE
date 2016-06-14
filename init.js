af001ad172c084fa01eeba63c1249278dd8c24d5
Milo Arnerich Hatch <miloshcavitch@gmail.com>
2016-06-14 14:28:35 -0400

    init commit

---
+++ init.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById('game-canvas');
+var ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
---
+++ starsBG.js
@@ -0,0 +1,106 @@
+var ahora = Date.now();// initialized here because this is first event
+var s = 1;
+function star(x){
+  switch(x){
+    case 16:
+      this.thesize = Math.random()*0.25 + 0.3;
+      this.speed = this.thesize * s;
+      break;
+    case 8:
+      this.thesize = Math.random()*0.5 + 0.6;
+      this.speed = this.thesize * s;
+      break;
+    case 4:
+      this.thesize = Math.random() * 1 + 1.2;
+      this.speed = this.thesize * s;
+      break;
+    case 2:
+      this.thesize = Math.random() * 2 + 2.4;
+      this.speed = this.thesize * s;
+      break;
+    case 1:
+      this.thesize = Math.random() * 4 + 4.8;
+      this.speed = this.thesize * s;
+      break;
+    default:
+      this.thesize = Math.random() * 4.8;
+      this.speed = this.thesize * s;
+  }
+  this.posY = -20;
+  this.posX = Math.round(Math.random() * canvas.width);
+  this.updateStar = function(){
+    ctx.beginPath();
+    ctx.rect(this.posX, this.posY, this.thesize, this.thesize * 1.5);
+    ctx.fillStyle = 'white';//colorRay[Math.round(Math.random() * 764)];// or colorRay[charColorIndex]; colorRay[charColorIndex];
+    ctx.fill();
+    ctx.closePath();
+    this.posY += this.speed * (velocity.starRate * 5 );
+  }
+}
+var starLayers = [];
+var starSpawnTimer = {spawnTimer: 0, then: 0};
+
+//new one
+
+function spacedSpawner(){
+    var spaceStat = Math.random() * 31;
+    var x;
+    if (spaceStat > 15){
+      x = 16;
+    }
+    if (spaceStat <= 15 && spaceStat > 7){
+      x = 8;
+    }
+    if (spaceStat <= 7 && spaceStat > 3){
+      x = 4;
+    }
+    if (spaceStat <= 3 && spaceStat > 1){
+      x = 2;
+    }
+    if (spaceStat <= 1){
+      x = 1;
+    }
+    starLayers.push(new star(x))
+}
+function starVelocityController(){
+  //console.log(vel.deltaSpeed);
+  for (var i = 0; i < velocity.deltaInt; i++){
+    spacedSpawner();
+  }
+}
+function LayerTraverse(el){//runs updateStar for all the stars
+    el.updateStar();
+    if (el.posY > canvas.height + el.thesize){
+      starLayers.splice(starLayers.indexOf(el), 1);
+    }
+}
+var fpsCounter = 0;
+var lastSecond = 0;
+var fpsString = '';
+
+function updateStars(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.rect(0, 0, canvas.width, canvas.height);
+  velocity.updateSpeed();
+//  spaceBackground();
+  ctx.fillStyle = 'black';//variable can be found in rainbowray.js
+  ctx.fill();
+  ctx.closePath();
+  starVelocityController();
+  starLayers.forEach(LayerTraverse);
+  //counts frames per second for debugging, should be 50
+  testTank.render();
+  tankTwo.render();
+  fpsCounter++;
+  if (Date.now() >= lastSecond + 1000){
+    fpString = fpsCounter + " FPS";
+    fpsCounter = 0;
+    lastSecond = Date.now();
+  }
+  ctx.font = "15px Arial"
+  ctx.fillText(fpString, 10, 20);
+
+}
+
+setInterval(updateStars, 20);
